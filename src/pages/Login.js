0161e44a42cee495958891c00f0a446a5a4bab11
Anaghbrati Sinha Ray <anaghbratisinharay2006@gmail.com>
2025-09-01 02:38:52 +0530

    add

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `${APIUrl}/login`; //change
+            const url = `${APIUrl}/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
