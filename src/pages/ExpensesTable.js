28cf8c982b21cf1fa0209a33e85d1d5c15488664
Anaghbrati Sinha Ray <anaghbratisinharay2006@gmail.com>
2025-09-09 23:23:47 +0530

    console

--- src/pages/ExpensesTable.js
+++ src/pages/ExpensesTable.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function ExpensesTable( {expenses , handleDeleteExpense} ) {
-    console.log('ExpensesTable-->', expenses)
+    // console.log('ExpensesTable-->', expenses) // it will print the expenses array in the console
     return (
         <div className="expense-List">
             {
